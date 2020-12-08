c2a12a80025b4528771a732cd19f967cabea5d82
Alicia Steen <asteen@hotels.com>
2020-12-08 11:59:23 +0000

    The rest of day two

--- day2/solution.go
+++ day2/solution.go
@@ -10,12 +10,15 @@ import (
 )
 
 func main() {
-	var count int
+	var countPart1, countPart2 int
 	input := readInput("day2/input")
 	for _, line := range input {
-		count += validate(line)
+		countPart1 += validatePart1(line)
+		countPart2 += validatePart2(line)
 	}
-	fmt.Println(count, "passwords meet requirements")
+	fmt.Println("Part 1: ", countPart1, "passwords meet requirements")
+	fmt.Println("Part 2: ", countPart2, "passwords meet requirements")
+
 }
 
 func readInput(path string) []string {
@@ -38,7 +41,7 @@ func readInput(path string) []string {
 	return input
 }
 
-func validate(input string) int {
+func validatePart1(input string) int {
 	s := strings.Split(input, " ")
 
 	low, _ := strconv.Atoi(strings.Split(s[0], "-")[0])
@@ -53,4 +56,30 @@ func validate(input string) int {
 		return 1
 	}
 	return 0
+}
+
+func validatePart2(input string) int {
+	s := strings.Split(input, " ")
+
+	positionOne, _ := strconv.Atoi(strings.Split(s[0], "-")[0])
+	positionOne = positionOne -1
+	positionTwo, _ := strconv.Atoi(strings.Split(s[0], "-")[1])
+	positionTwo = positionTwo -1
+
+	character := s[1][:1]
+	password := s[2]
+
+	var count int
+	if string(password[positionOne]) == character {
+		count++
+	}
+	if string(password[positionTwo]) == character {
+		count++
+	}
+
+	if count == 1 {
+		return 1
+	} else {
+		return 0
+	}
 }
