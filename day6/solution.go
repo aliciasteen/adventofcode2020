5558ef968ede277e38b8ec5f52dffa9aab2fe470
Alicia Steen <asteen@hotels.com>
2020-12-09 11:49:02 +0000

    Tidying day 6

--- day6/solution.go
+++ day6/solution.go
@@ -3,16 +3,14 @@ package main
 import (
 	"adventofcode2020/utils"
 	"fmt"
-	"sort"
 	"strings"
 )
 
 func main() {
 	input := utils.ReadInputMultiLine("day6/input", "")
-
-	sum := 0
-	for _, line := range input {
-		_, questions := processGroup(line)
+ 	sum := 0
+	for _, group := range input {
+		_, questions := processGroup(group)
 		sum += questions
 	}
 	fmt.Println("Number of questions anwsered yes:", sum)
@@ -20,15 +18,10 @@ func main() {
 
 func processGroup(input string) (numberOfPeople, numberOfQuestions int) {
 	people := strings.Fields(input)
-	sort.Strings(people)
 	input = strings.ReplaceAll(input, " ", "")
-	questions := make(map[string]int)
+	questions := make(map[rune]int)
 	for _, char := range input {
-		if _, exists := questions[string(char)]; exists {
-			questions[string(char)]++
-		} else {
-			questions[string(char)] = 1
-		}
+		questions[char]++
 	}
 	everyoneAnwsered := 0
 	for _, value := range questions {
